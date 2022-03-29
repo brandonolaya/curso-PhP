e72f102a2d20debe9866169db0308672dad0c2e6
Brandon Olaya <bvolaya1@gmail.com>
2022-03-29 00:21:06 -0500

    use php and html

--- function/game.php
+++ function/game.php
@@ -1,31 +1,72 @@
 <?php
-$possible_words = array("hola", "Holli");
-do {
-    array_push($possible_words, (string)readline ( "write your word: "));
-    echo "Do you finished to write the words?" . 
-    "\n" . "1. for continue." . "\n" . "2. for leave" . "\n";
-    $leave = (int) readline();
-    switch($leave){
-        case 1:
-            $leave = 1;
-            break;
-        default:
-            $leave = 0;
+function clear(){
+    if (PHP_OS === "WINNT"){
+        system ("cls");
     }
-} while ($leave == 1);
-$numbers_try = 1;
-$numbers_try = (int) readline ( "How many attempts can you have?: " );
+    else{
+        system ("clear");
+    }
+}
+
+
+$possible_words = ["fruits", "meals", "back", "color", "computer", 
+                    "lap-top", "girls", "phone","soccer" ];
+
+define("MAX_ATTEMPS", 8);
 
 echo "  ¡Games of hanged! \n\n";
 //game start
-$number = count($possible_words);
-$choosen_word = $possible_words[ rand(1, $number) ];
+//try the words
+$choosen_word = $possible_words[ rand(0, 9) ];
+
 //only lowercase
 $choosen_word = strtolower($choosen_word);
+
 //Since the word
 $word_length = strlen($choosen_word);
-$discovered_letters = str_pad(" ", $word_length, "_");
+//str_pag: full the string
+$discovered_letters = str_pad("", $word_length, "_");
+$attempts =0;
 
-echo $discovered_letters;
+
+do{
+    echo "Palabra de " . $word_length . " letras " . "\n\n";
+    echo $discovered_letters . "\n\n";
+
+    //Write user a letter
+    $player_latter = readline("Write a letter: ");
+    $player_latter = strtolower($player_latter);
+
+    //validate
+    if( str_contains($choosen_word, $player_latter) ){
+        //occurrences of the latter to replace it
+        $offset = 0;
+        while(
+            ($letter_pasition = strpos($choosen_word, $player_latter, 
+            $offset) ) !== false 
+        ){
+            $discovered_letters [$letter_pasition] = $player_latter;
+            $offset = $letter_pasition + 1;
+
+        }
+    }
+    else{
+        clear();
+        $attempts++;
+        echo "letter incorrect, You have " .(MAX_ATTEMPS - $attempts). " trys";
+        sleep(2);
+    }
+    clear();
+}while($attempts < MAX_ATTEMPS && $discovered_letters != $choosen_word);
+clear();
+
+if ($attempts < MAX_ATTEMPS) {
+    echo "¡¡You WINNNN!!"
+}
+else{
+    echo "¡Lunk for soon, JAJAJAJA!"
+}
+echo "The word is: " . $choosen_word . "\n";
+echo "You found: " . $discovered_letters . "\n";
 echo "\n";
 ?>
